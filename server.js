633bcffba9707615c08c44dfb95d4cde97471fcb
Ricardo Urbaez <Rickyurbaez2@gmail.com>
2026-07-18 19:44:30 -0400

    Prepare Railway deployment

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
+
 /** Get first non-internal IPv4 for LAN URL */
 function getLanUrl(port) {
   const portNum = port || 3000;
